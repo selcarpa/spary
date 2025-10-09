4aba63cb93aca767edc3b78927bb41910fba0a45
selcarpa <selcarpa@gmail.com>
2025-10-09 16:49:03 +0800

    feat(ui): Restructure the homepage layout and optimize the logo design
    - Restructure the page layout using Vuetify components
    - Add temporary SVG icons to replace the default graphics

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <Spary />
+  <v-card class="fill-height">
+    <v-layout class="fill-height">
+      <v-navigation-drawer
+          expand-on-hover
+          permanent
+          rail
+      >
+        <v-list>
+          <v-list-item
+              prepend-avatar="/src/assets/logo.svg"
+              subtitle=""
+              title="Spary"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-just-nothing" title="🌊" value="spary"></v-list-item>
+          <v-list-item prepend-icon="mdi-cog" title="Settings" value="settings"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 100vh">
+        <Spary />
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
